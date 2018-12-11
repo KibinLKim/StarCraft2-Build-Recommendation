95c0221e88fc34c48af544df8f3615bef1991301
edin <vlfdus0401@khu.ac.kr>
2018-12-11 11:08:18 +0900

    add code to upload images

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 var http = require('http');
 var fs = require('fs');
+var express=require('express');
+var app=express();
+
+app.get('/', function(req,res){
+  fs.readFile('index.html',function(err,data){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(data);
+  });
+});
+
+app.get('/imgs',function(req,res){
+  fs.readFile('logoIMG.jpg',function(err,data){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(data);
+  });
+});
+
+app.listen(8080,function(){
+  console.log('Server Start.');
+})
+/*
 http.createServer(function (req, res) {
   fs.readFile('index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -7,3 +28,4 @@ http.createServer(function (req, res) {
     res.end();
   });
 }).listen(8080);
+*/
